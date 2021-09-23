7077735625dea86d8144df61273c77fb351a0ccb
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-09-23 09:14:47 +0200

    Update audio.sh

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -20,21 +20,21 @@ clear ; echo "Checking for USB Mics. Please have only 1 USB Mic/soundcard connec
 arecord -q --list-devices | grep -m 1 -q 'USB Microphone\|USB\|usb\|Usb\|Microphone\|MICROPHONE\|microphone\|mic\|Mic\|MIC' 
 if [ $? -eq 0 ]
 then
-	echo
-        echo -e "|"  "${IGreen}USB Microphone detected! ${Color_Off} |"
+	echo ; echo -e "|"  "${IGreen}USB Microphone detected! ${Color_Off} |"
 else
         echo -e "|"  "${IRed}No USB Microphone detected! Please plug one in now, and restart! ${Color_Off} |" >&2
         #LED/beep that mic is not detected
+	# sleep 10 && reboot
         exit 1
 fi
 sleep 4
 
 ##################################### Set volume and unmute
 echo ; echo "Set volume and unmute" ; echo 
-amixer -q -c $CARD set Mic 100% unmute
+amixer -q -c $CARD set Mic 80% unmute
 if [ $? -eq 0 ]
 then
-	echo -e "|"  "${IGreen}Mic input volume set to 100% and is unmuted${Color_Off} |"
+	echo -e "|"  "${IGreen}Mic input volume set to 80% and is unmuted${Color_Off} |"
 else
         echo -e "|"  "${IRed}Failed to set input volume${Color_Off} |" >&2
         #exit 1
@@ -66,18 +66,28 @@ sleep 4
 ##################################### Test playback
 echo ; echo "Testing playback of the recording" ; echo
 aplay /tmp/test-mic.wav
-if [ $? -eq 0 ]
-then
-	echo
-        echo -e "|"  "${IGreen}Playback is ok! ${Color_Off} |"
+if [ $? -eq 0 ]; then
+	echo ; echo -e "|"  "${IGreen}Playback is ok! ${Color_Off} |"
 	rm -r /tmp/test-mic.wav
 else
-	echo
-        echo -e "|"  "${IRed}Playback failed! ${Color_Off} |" >&2
+	echo ; echo -e "|"  "${IRed}Playback failed! ${Color_Off} |" >&2
 	rm -r /tmp/test-mic.wav
         #exit 1
 fi
 sleep 4
 
+##################################### Check USB drives
+#The fundamental storage of the recordings is to live on attached USB (thumb) drives. If up to two or three USB drives are attached, then these are to be turned into a kind of (ZFS) RAID (perhaps a mirror configuration) for redundancy. The intended design is to have the "system" (application and OS) live on the original SD card (the "image") and then to have the payload (recordings) be stored on external USB. Perhaps an extra "backup" might also live on the SD card "just in case", although ideally encrypted at some point.
+#UUID check
+#If USB drive is true
+#Set write path to USB
+#else 
+#Set write path to SDcard
+##################################### Check if storage is writable and freespace is more than X
+#if "$ZFSTORAGE" is writable
+#check freespace "$ZFSTORAGE" 
+##################################### Backup recordings
+#zfs snapshot "$POOL"@"$DATE"-script-backup
+#rsync -aAXHv "$USB"/ "$LOCALSTORAGE"/
 ##################################### Finished
 exit 0
