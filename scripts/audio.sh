4677c1602e785229b462bf06c9200432b000db19
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-14 21:03:20 +0200

    Update audio.sh

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -17,7 +17,8 @@ root_check
 
 ##################################### Stop all recordings just to be sure
 if [ -f /tmp/.recording.lock ]; then
-	ps -cx -o pid,command | awk '$2 == "arecord" { print $1 }' | xargs kill -INT
+	ps -cx -o pid,command | awk '$2 == "arecord" { print $1 }' | xargs kill -INT | wait
+	sleep 5
 	rm /tmp/.recording.lock
 fi
 
