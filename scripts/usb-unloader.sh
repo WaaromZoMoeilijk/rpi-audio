985cd5903260d8ace9fcf155a875c8769d21f8a9
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-14 21:30:40 +0200

    Update usb-unloader.sh

--- scripts/usb-unloader.sh
+++ scripts/usb-unloader.sh
@@ -14,6 +14,12 @@
 source <(curl -sL https://raw.githubusercontent.com/WaaromZoMoeilijk/rpi-audio/main/lib.sh)
 
 ################################### Check for errors + debug code and abort if something isn't right
+debug_mode() {
+if [ "$DEBUG" -eq 1 ]
+then
+    set -ex
+fi
+}
 # 1 = ON | 0 = OFF
 DEBUG=1
 debug_mode
@@ -28,7 +34,7 @@ GITDIR='/opt/rpi-audio'
 
 ################################### check for defined log file
 if [ -z "$LOG_FILE" ]; then
-    echo -e "|" "${IRed}No log file${Color_Off} |" >&2
+    echo ; echo -e "|" "${IRed}No log file${Color_Off} |" >&2
     exit 1
 fi
 
@@ -37,36 +43,23 @@ autounload() {
 	echo ; echo -e "|" "${IBlue} --- USB UnLoader --- ${Color_Off} |" >&2 ; echo    
 
     if [ -z "$MOUNT_DIR" ]; then
-	    echo -e "|" "${IRed}Failed to supply Mount Dir parameter${Color_Off} |" >&2
+	     echo ; echo -e "|" "${IRed}Failed to supply Mount Dir parameter${Color_Off} |" >&2
         exit 1
     fi
 
     if [ -z "$DEVICE" ]; then
-	    echo -e "|" "${IRed}Failed to supply DEVICE parameter${Color_Off} |" >&2
+	     echo ; echo -e "|" "${IRed}Failed to supply DEVICE parameter${Color_Off} |" >&2
         exit 1
     fi
-
-    # Unmount device
-    umount "$MOUNT_DIR/$DEVICE" && echo -e "|" "${IGreen}umount - Done${Color_Off} |" >&2 || echo -e "|" "${IRed}Umount - Failed${Color_Off} |" >&2
-    systemd-umount "$MOUNT_DIR/$DEVICE" && echo -e "|" "${IGreen}Systemd-unmount - Done${Color_Off} |" >&2 || echo -e "|" "${IRed}Systemd-umount - Failed${Color_Off} |" >&2
-    systemctl disable "$MOUNT_DIR-$DEVICE".mount && echo -e "|" "${IGreen}Systemctl disabled $MOUNT_DIR-$DEVICE.mount - Done${Color_Off} |" >&2 || echo -e "|" "${IRed}Systemctl disabled $MOUNT_DIR-$DEVICE.mount - Failed${Color_Off} |" >&2
-    systemctl daemon-reload && echo -e "|" "${IGreen}Systemctl daemon-reload - Done${Color_Off} |" >&2 || echo -e "|" "${IRed}Systemctl daemon-reload - Failed${Color_Off} |" >&2
-    systemctl reset-failed && echo -e "|" "${IGreen}Systemctl reset-failed - Done${Color_Off} |" >&2 || echo -e "|" "${IRed}Systemctl reset-failed - Failed${Color_Off} |" >&2
-
-    # Wait for a second to make sure async umount has finished
-    sleep 2
-
-    # Remove folder after unmount
-    rmdir "$MOUNT_DIR/$DEVICE" && echo -e "|" "${IGreen}$MOUNT_DIR/$DEVICE folder removed${Color_Off} |" >&2 || echo -e "|" "${IRed}$MOUNT_DIR/$DEVICE folder remove - Failed${Color_Off} |" >&2
 
     # test that this device has disappeared from mounted devices
     device_mounted=$(grep "$DEVICE" /etc/mtab)
     if [ "$device_mounted" ]; then
-        echo -e "|" "${IRed}/dev/$DEVICE failed to Un-Mount, forcing umount -l${Color_Off} |" >&2
+         echo ; echo -e "|" "${IRed}/dev/$DEVICE failed to Un-Mount, forcing umount -l${Color_Off} |" >&2
 	    umount -l "/dev/$DEVICE"
         #exit 1
     else
-        echo -e "|" "${IGreen}/dev/$DEVICE successfully Un-Mounted${Color_Off} |" >&2
+         echo ; echo -e "|" "${IGreen}/dev/$DEVICE successfully Un-Mounted${Color_Off} |" >&2
     fi
 }
 
