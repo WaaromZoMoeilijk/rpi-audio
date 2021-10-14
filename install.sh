187110a0f30b37ab3bcb9ed0f6bfe285d8386ccd
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-14 20:56:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -256,7 +256,7 @@ if [ -f "/etc/udev/rules.d/85-usb-loader.rules" ]; then
 else
 
 cat >> /etc/udev/rules.d/85-usb-loader.rules <<EOF
-ACTION=="add", KERNEL=="sd*[0-9]", SUBSYSTEMS=="usb", RUN+="$GITDIR/scripts/usb-initloader.sh ADD %k $env{ID_FS_TYPE}"
+ACTION=="add", KERNEL=="sd*[0-9]", SUBSYSTEMS=="usb", RUN+="$GITDIR/scripts/usb-initloader.sh ADD %k \$env{ID_FS_TYPE}"
 ACTION=="remove", KERNEL=="sd*[0-9]", SUBSYSTEMS=="usb", RUN+="$GITDIR/scripts/usb-initloader.sh %k"
 EOF
 
