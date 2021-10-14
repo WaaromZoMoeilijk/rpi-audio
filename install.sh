0e629d41b0de9ba247c7bed70544bc9f3880ca6f
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-14 13:16:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,24 +12,24 @@ source <(curl -sL https://raw.githubusercontent.com/WaaromZoMoeilijk/rpi-audio/m
 ###################################  Check for errors + debug code and abort if something isn't right
 # 1 = ON
 # 0 = OFF
-DEBUG=1
+DEBUG=0
 debug_mode
 
 ###################################  Check if script runs as root
 root_check
-
+clear
 ################################### Prefer IPv4 for apt
-echo ; echo -e "|" "${IBlue}IPv4 APT${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue}IPv4 APT Preference${Color_Off} |" >&2 ; echo
 if [ -f /etc/apt/apt.conf.d/99force-ipv4 ]; then
 	echo -e "|" "${IYellow}IPv4 Preference already set${Color_Off} |" >&2
 else
 	echo 'Acquire::ForceIPv4 "true";' >> /etc/apt/apt.conf.d/99force-ipv4
 	if [ $? -eq 0 ]; then
-		echo -e "|" "${IGreen}Set IPv4 Preference - Done${Color_Off} |" >&2	
+		echo -e "|" "${IGreen}Set IPv4 Preference - Done${Color_Off} |" >&2
 	else
-		echo -e "|" "${IRed}Set IPv4 Preference - Failed${Color_Off} |" >&2	
+		echo -e "|" "${IRed}Set IPv4 Preference - Failed${Color_Off} |" >&2
 	fi
-fi 
+fi
 
 ################################### Upstart
 echo ; echo -e "|" "${IBlue}Setting up rc.local - $DATE${Color_Off} |" >&2 ; echo
@@ -37,10 +37,11 @@ if [ -f "/etc/rc.local" ]; then
       mv /etc/rc.local /etc/rc.local.backup
 fi
 
-if cat /etc/rc.local | grep -q "$GITDIR/install.sh"; then
-	echo -e "|" "${IYellow}Setting up rc.local - Already exists${Color_Off} |" >&2
-else
-echo "Adding RC.local"
+if [ -f /etc/rc.local ]; then
+      	mv /etc/rc.local /etc/rc.local.backup
+	if cat /etc/rc.local | grep -q "$GITDIR/install.sh"; then
+		echo -e "|" "${IYellow}Setting up rc.local - Already exists${Color_Off} |" >&2
+	else
 cat > /etc/systemd/system/rc-local.service <<EOF
 [Unit]
 Description=/etc/rc.local
@@ -63,30 +64,39 @@ cat > /etc/rc.local <<EOF
 exit 0
 EOF
 
-	# Check if the above is succesfull 
-	if cat /etc/rc.local | grep -q "$GITDIR/install.sh"; then
-		echo -e "|" "${IGreen}Setting up rc.local - Exists${Color_Off} |" >&2
-	else
-		echo -e "|" "${IRed}Setting up rc.local - Failed${Color_Off} |" >&2
+		if [ -f /etc/rc.local ]; then
+			# Check if the above is succesfull
+			if cat /etc/rc.local | grep -q "$GITDIR/install.sh"; then
+				echo -e "|" "${IGreen}Setting up rc.local - Exists${Color_Off} |" >&2
+			else
+				echo -e "|" "${IRed}Setting up rc.local - Failed, file exists but not the proper content${Color_Off} |" >&2
+			fi
+		else
+			echo -e "|" "${IRed}Setting up rc.local - Failed${Color_Off} |" >&2
+		fi
+
+		systemctl daemon-reload
+		systemctl enable rc-local.service
+		systemctl start rc-local.service
 	fi
 fi
 
 #################################### Update
-echo ; echo -e "|" "${IBlue}Update${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue} ==> Update: OS <==  - $DATE${Color_Off} |" >&2 ; echo
 export "DEBIAN_FRONTEND=noninteractive"
 export "DEBIAN_PRIORITY=critical"
-clear ; echo "Auto clean"
+echo -e "|"  "${IBlue}Auto clean${Color_Off} |" >&2
 apt_autoclean & spinner
-clear ; echo "Auto remove"
+echo -e "|"  "${IBlue}Auto purge${Color_Off} |" >&2
 apt_autoremove & spinner
-clear ; echo "Update"
+echo -e "|"  "${IBlue}Update${Color_Off} |" >&2
 apt_update & spinner
-clear ; echo "Upgrade"
+echo -e "|"  "${IBlue}Upgrade${Color_Off} |" >&2
 apt_upgrade & spinner
 
 ################################### Dependencies
-echo ; echo -e "|" "${IBlue}Dependancies${Color_Off} |" >&2 ; echo
-apt_install \
+echo -e "|" "${IBlue}Dependancies${Color_Off} |" >&2
+apt-get install -y -qq -o=Dpkg::Use-Pty=0 \
         git \
         jq \
         nano \
@@ -98,28 +108,32 @@ apt_install \
         ufw \
         rsyslog \
         fail2ban \
-	dbus
-#	gpgv1 
+	dbus \
+	lshw \
+	ufw
+#	gpgv1
 #  	zerotier
 #	autossh \
-#	raspberrypi-kernel-headers \	
-	
+#	raspberrypi-kernel-headers \
+
 ################################### VDMFEC
-echo ; echo -e "|" "${IBlue}VMDFEC${Color_Off} |" >&2 ; echo
-if apt list vdmfec | grep -q installed; then
-	echo -e "|" "${IYellow}vdmfec is already installed${Color_Off} |" >&2 
+echo ; echo -e "|" "${IBlue}VMDFEC - $DATE${Color_Off} |" >&2 ; echo
+apt list vdmfec > /tmp/.vdm 2>&1 || true
+if echo $(cat /tmp/.vdm) | grep -q installed; then
+	echo -e "|" "${IYellow}vdmfec is already installed${Color_Off} |" >&2
 else
 	# 64Bit change for other arm distros
 	wget http://ftp.de.debian.org/debian/pool/main/v/vdmfec/vdmfec_1.0-2+b2_arm64.deb && dpkg -i vdmfec_1.0-2+b2_arm64.deb && rm vdmfec_1.0-2+b2_arm64.deb
-	if apt list vdmfec | grep -q installed; then
+	apt list vdmfec > /tmp/.vdm 2>&1 || true
+	if echo $(cat /tmp/.vdm) | grep -q installed; then
 		echo -e "|" "${IGreen}vdmfec install - Done${Color_Off} |" >&2
 	else
 		echo -e "|" "${IRed}vdmfec install - Failed${Color_Off} |" >&2
-	fi	
+	fi
 fi
 
-################################### Set timezone based upon WAN ip 
-echo ; echo -e "|" "${IBlue}Set timezone based on WAN IP${Color_Off} |" >&2 ; echo
+################################### Set timezone based upon WAN ip
+echo ; echo -e "|" "${IBlue}Set timezone based on WAN IP - $DATE${Color_Off} |" >&2 ; echo
 
 timedatectl set-timezone Europe/Amsterdam &>/tmp/.tz || true
 if echo $(cat /tmp/.tz) | grep -q "Failed to connect to bus: No such file or directory"; then
@@ -131,7 +145,7 @@ else
 			echo -e "|" "${IGreen}Timezone set!${Color_Off} |" >&2
 		else
 			echo -e "|" "${IRed}Timezone set failed!${Color_Off} |" >&2
-		fi	
+		fi
 	else
 		curl -sL 'ip-api.com/json' | jq '.timezone' | xargs timedatectl set-timezone
 		if [ $? -eq 0 ]; then
@@ -143,12 +157,12 @@ else
 fi
 
 ################################### Allow access, temp during dev
-echo ; echo -e "|" "${IBlue}Dev access${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue}Dev access - $DATE${Color_Off} |" >&2 ; echo
 if [ -d "/root/.ssh" ]; then
 	echo -e "|" "${IGreen}Folder .ssh exists!${Color_Off} |" >&2
 else
 	mkdir -p /root/.ssh
-	echo -e "|" "${IGreen}Folder /root/.ssh created!${Color_Off} |" >&2	
+	echo -e "|" "${IGreen}Folder /root/.ssh created!${Color_Off} |" >&2
 fi
 
 if cat /root/.ssh/autorized_keys | grep -q "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1ME48x4opi86nCvc6uT7Xz4rfhzR5/EGp24Bi/C21UOyyeQ3QBIzHSSBAVZav7I8hCtgaGaNcIGydTADqOQ8lalfYL6rpIOE3J4XyReqykLJebIjw9xXbD4uBx/2KFAZFuNybCgSXJc1XKWCIZ27jNpQUapyjsxRzQD/vC4vKtZI+XzosqNjUrZDwtAqP74Q8HMsZsF7UkQ3GxtvHgql0mlO1C/UO6vcdG+Ikx/x5Teh4QBzaf6rBzHQp5TPLWXV+dIt0/G+14EQo6IR88NuAO3gCMn6n7EnPGQsUpAd4OMwwEfO+cDI+ToYRO7vD9yvJhXgSY4N++y7FZIym+ZGz"; then
@@ -163,7 +177,7 @@ else
 fi
 
 ################################### Create user
-echo ; echo -e "|" "${IBlue}Creating user${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue}Creating user - $DATE${Color_Off} |" >&2 ; echo
 if cat /etc/passwd | grep "$USERNAME"; then
 	echo -e "|" "${IGreen}User exists!${Color_Off} |" >&2
 else
@@ -176,15 +190,22 @@ else
 fi
 
 ################################### Clone git repo
-echo ; echo -e "|" "${IBlue}Clone git repo${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue}Clone git repo - $DATE${Color_Off} |" >&2 ; echo
 if [ -d "$GITDIR" ]; then
-	rm -r "$GITDIR" # Only during dev
 	#cd "$GITDIR"
 	#git pull
+	rm -r "$GITDIR"
 	if [ $? -eq 0 ]; then
 		echo -e "|" "${IGreen}Git repository removed!${Color_Off} |" >&2
 	else
 		echo -e "|" "${IRed}Git repository failed to remove!${Color_Off} |" >&2
+	fi
+
+	git clone "$REPO" "$GITDIR"
+	if [ $? -eq 0 ]; then
+		echo -e "|" "${IGreen}Git repository cloned!${Color_Off} |" >&2
+	else
+		echo -e "|" "${IRed}Git repository failed to clone!${Color_Off} |" >&2
 	fi
 else
 	git clone "$REPO" "$GITDIR"
@@ -192,32 +213,31 @@ else
 		echo -e "|" "${IGreen}Git repository cloned!${Color_Off} |" >&2
 	else
 		echo -e "|" "${IRed}Git repository failed to clone!${Color_Off} |" >&2
-	fi	
+	fi
 fi
-
 ################################### Hardening
-echo ; echo -e "|" "${IBlue}Hardening${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue}Hardening - $DATE${Color_Off} |" >&2 ; echo
 /bin/bash "$GITDIR"/scripts/hardening.sh
 if [ $? -eq 0 ]; then
 	echo -e "|" "${IGreen}Hardening executed!${Color_Off} |" >&2
 else
 	echo -e "|" "${IRed}Hardening failed!${Color_Off} |" >&2
-fi	
+fi
 ################################### Dynamic overclock
 # Please at minimum add some heat sinks to the RPI. Better to also add a FAN. thermal throtteling is in place at 75 celcius 
 # Overclocking dynamically will only affect the temp on high load for longer periods. You can mitigate that with above.
-echo ; echo -e "|" "${IBlue}Overclock${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue}Overclock - $DATE${Color_Off} |" >&2 ; echo
 if cat /proc/cpuinfo | grep -q "Raspberry Pi 4"; then
 	/bin/bash "$GITDIR"/scripts/overclock.sh
 	if [ $? -eq 0 ]; then
 		echo -e "|" "${IGreen}Overclock set, active on next reboot. Press shift during boot to disable!${Color_Off} |" >&2
 	else
 		echo -e "|" "${IRed}Overclock set failed!${Color_Off} |" >&2
-	fi	    
+	fi
 fi
 
 ################################### Storage, add auto mount & checks for usb drives
-echo ; echo -e "|" "${IBlue}Storage${Color_Off} |" >&2 ; echo
+echo ; echo -e "|" "${IBlue}Storage - $DATE${Color_Off} |" >&2 ; echo
 if [ -f "/etc/udev/rules.d/85-usb-loader.rules" ]; then
 	echo -e "|"  "${IGreen}/etc/udev/rules.d/85-usb-loader.rules exists${Color_Off} |" >&2
 else
@@ -233,22 +253,20 @@ EOF
 fi
 
 ################################### UPS
-#echo ; echo -e "|" "${IBlue}UPS${Color_Off} |" >&2 ; echo
+#echo ; echo -e "|" "${IBlue}UPS - $DATE${Color_Off} |" >&2 ; echo
 #/bin/bash "$GITDIR"/scripts/ups.sh
 
 ################################### ZeroTier
-#echo ; echo -e "|" "${IBlue}Zerotier/networking${Color_Off} |" >&2 ; echo
+#echo ; echo -e "|" "${IBlue}Zerotier/networking - $DATE${Color_Off} |" >&2 ; echo
 #/bin/bash "$GITDIR"/scripts/zerotier.sh
 
 ################################### LED / Buttons
-#echo ; echo -e "|" "${IBlue}LED/buttons${Color_Off} |" >&2 ; echo
+#echo ; echo -e "|" "${IBlue}LED/buttons - $DATE${Color_Off} |" >&2 ; echo
 #/bin/bash "$GITDIR"/scripts/ph.sh
 
 ################################### Audio recording
-echo ; echo -e "|" "${IBlue}Audio${Color_Off} |" >&2 ; echo
-echo -e "|" "${IBlue} Audio recording - $DATE${Color_Off} |" > "$LOG_FILE_AUDIO"
+echo ; echo -e "|" "${IBlue}Audio - $DATE${Color_Off} |" >&2 ; echo
+echo -e "|" "${IBlue}Audio recording - $DATE${Color_Off} |" > "$LOG_FILE_AUDIO"
 /bin/bash "$GITDIR"/scripts/audio.sh >> "$LOG_FILE_AUDIO" 2>&1&
 
-clear
-
-exit 0
+#exit 0
