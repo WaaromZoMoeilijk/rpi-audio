09fd016ec2ab98430d16b3102bdd383fceb77d9c
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-15 09:59:17 +0200

    Update hardening.sh

--- scripts/hardening.sh
+++ scripts/hardening.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 # fail2ban install
-dietpi-software install 73
+apt list fail2ban > /tmp/.f2b 2>&1 || true
+if echo $(cat /tmp/.f2b) | grep -q installed; then
+	echo -e "|" "${IYellow}Fail2ban is already installed${Color_Off} |" >&2
+else
+  apt install fail2ban -y
+  
+  # Fail2ban jails
+  wget -O /etc/fail2ban/jail.local https://raw.githubusercontent.com/WaaromZoMoeilijk/rpi-audio/main/static/jail.local
+
+  systemctl restart fail2ban
+
+	apt list fail2ban > /tmp/.f2b 2>&1 || true
+	if echo $(cat /tmp/.f2b) | grep -q installed; then
+		echo ; echo -e "|" "${IGreen}Fail2ban install - Done${Color_Off} |" >&2
+	else
+		echo ; echo -e "|" "${IRed}Fail2ban install - Failed${Color_Off} |" >&2
+	fi
+fi
 
 # UFW
 echo "y" | ufw reset
@@ -9,10 +26,5 @@ ufw default allow outgoing
 ufw default deny incoming
 ufw limit 22/tcp
 echo "y" | ufw enable
-
-# Fail2ban
-wget -O /etc/fail2ban/jail.local https://raw.githubusercontent.com/WaaromZoMoeilijk/rpi-audio/main/static/jail.local
-
-systemctl restart fail2ban
 
 exit 0
