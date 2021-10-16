499138292eeb40db8ea574cae08c4567767d6970
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-16 20:08:05 +0200

    Update audio.sh

--- scripts/audio.sh
+++ scripts/audio.sh
@@ -33,17 +33,17 @@ echo $(date) > /tmp/.recording.lock
 # Implement a check for double drives.
 header "Checking for USB drives." 
 if [[ $(find /mnt -iname '.active' | sed 's|/.active||g') ]]; then
-        MNTPT=$(find /mnt -iname '.active' | sed 's|/.active||g')
-        success "Active drive has been found, proceeding"
+    MNTPT=$(find /mnt -iname '.active' | sed 's|/.active||g')
+    success "Active drive has been found, proceeding"
 else
-        fatal "No active drive has been found, please reinsert or format USB"
+    fatal "No active drive has been found, please reinsert or format USB"
 fi
 
 ##################################### Check if storage is writable
 header "Checking if the storage is writable." 
 touch "$MNTPT"/.test
 if [ -f "$MNTPT/.test" ]; then
-	success "Storage is writable!"
+	success "Storage is writable"
 	rm "$MNTPT"/.test
 else
 	error "Storage is not writable, exiting."
@@ -53,70 +53,69 @@ fi
 ##################################### Check free space
 header "Checking free space available on root." 
 if [ "$LOCALSTORAGEUSED" -le 2000 ]; then
-        echo ; error "Less then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
+    error "Less then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
 else
-        echo ; success "More then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
+    success "More then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
 fi      
 
 header "Checking free space available on storage." 
 if [ $USEP -ge '90' ]; then
 	error "Drive has less then 10% storage capacity available, please free up space."
 else
-	success "Drive has more then 10% capacity available, proceeding!"
+	success "Drive has more then 10% capacity available, proceeding"
 fi
 
 if [ $(df -Ph -BM $MNTPT | tail -1 | awk '{print $4}' | sed 's|M||g') -le 2000 ]; then
-        echo  "Less then 2000MB available on usb storage directory: $USEM MB (USB)"
-        exit 1
+    fatal "Less then 2000MB available on usb storage directory: $USEM MB (USB)"
 else
-        echo  "More then then 2000MB available on usb storage directory: $USEMMB (USB)"
+    success "More then then 2000MB available on usb storage directory: $USEMMB (USB)"
 fi
 
 ##################################### Check for USB Mic
-header "Checking for USB Mics. Please have only 1 USB Mic/soundcard connected!" 
+header "Checking for USB Mics. Please have only 1 USB Mic/soundcard connected" 
 arecord -q --list-devices | grep -m 1 -q 'USB Microphone\|USB\|usb\|Usb\|Microphone\|MICROPHONE\|microphone\|mic\|Mic\|MIC' 
 if [ $? -eq 0 ]; then
-	    echo ; success "USB Microphone detected!"
+	success "USB Microphone detected"
 else
-        fatal "No USB Microphone detected! Please plug one in now, and restart!"
-        #LED/beep that mic is not detected
-	    # sleep 10 && reboot
+    fatal "No USB Microphone detected! Please plug one in now, and restart"
+    #LED/beep that mic is not detected
+	# sleep 10 && reboot
 fi
 
 ##################################### Set volume and unmute
 header "Set volume and unmute" 
 amixer -q -c $CARD set Mic 80% unmute
 if [ $? -eq 0 ]; then
-		success "Mic input volume set to 80% and is unmuted"
+	success "Mic input volume set to 80% and is unmuted"
 else
-        fatal "Failed to set input volume"
+    fatal "Failed to set input volume"
 fi
 
 ##################################### Test recording
 header "Test recording"
 arecord -q -f S16_LE -d 3 -r 48000 --device="hw:$CARD,0" /tmp/test-mic.wav 
 if [ $? -eq 0 ]; then
-        success "Test recording is done!"
+    success "Test recording is done"
 else
-        fatal "Test recording failed!"
+    fatal "Test recording failed"
 fi
 
 ##################################### Check recording file size
 header "Check if recording file size is not 0" 
 if [ -s /tmp/test-mic.wav ]; then
-        success "File contains data!"
+    success "File contains data"
 else
-        error "File is empty! Unable to record."
+    error "File is empty! Unable to record."
 fi
 
 ##################################### Test playback
 header "Testing playback of the recording"
 aplay /tmp/test-mic.wav
 if [ $? -eq 0 ]; then
-	echo ; success "Playback is ok!"
+	success "Playback is ok"
 	rm -r /tmp/test-mic.wav
 else
-	echo ; error "Playback failed!"
+	error "Playback failed"
 	rm -r /tmp/test-mic.wav
 fi
 
@@ -139,15 +138,15 @@ tee "$MNTPT/$(date '+%Y-%m-%d')/$FILEDATE.wav.gpg"
 clear
 sleep 3
 
-if [ -f "$MNTPT/$FILEDATE.wav.gpg" ]; then
+if [ -f "$MNTPT/$(date '+%Y-%m-%d')/$FILEDATE.wav.gpg" ]; then
 	success "Recording is done"
 else
 	error "Something went wrong during the recording flow"
 fi
 
 # Reverse Pipe
-vdmfec -d -v -b "$BLOCKSIZE" -n 32 -k 24 "$MNTPT/"$FILEDATE".wav.gpg" | \
-gpg1 --homedir /root/.gnupg --decrypt > "$MNTPT/"$FILEDATE".decrypted.wav"
+vdmfec -d -v -b "$BLOCKSIZE" -n 32 -k 24 "$MNTPT/$(date '+%Y-%m-%d')/$FILEDATE.wav.gpg" | \
+gpg1 --homedir /root/.gnupg --decrypt > "$MNTPT/$(date '+%Y-%m-%d')/$FILEDATE.decrypted.wav"
 
 # SIGINT arecord - control + c equivilant. Used to end the arecord cmd and continue the pipe. Triggered when UPS mains is unplugged.
 #pkill -2 'arecord'
@@ -162,13 +161,13 @@ gpg1 --homedir /root/.gnupg --decrypt > "$MNTPT/"$FILEDATE".decrypted.wav"
 
 ###################################### Create par2 files
 # Implement a last modified file check for the latest recording only
-par2 create "$MNTPT/$FILEDATE.wav.gpg.par2" "$MNTPT/$FILEDATE.wav.gpg" && success "Par2 file created" || error "Failed to create Par2 file"
+par2 create "$MNTPT/$(date '+%Y-%m-%d')/$FILEDATE.wav.gpg.par2" "$MNTPT/$(date '+%Y-%m-%d')/$FILEDATE.wav.gpg" && success "Par2 file created" || error "Failed to create Par2 file"
 
 ###################################### Verify par2 files
-if [[ $(par2 verify "$MNTPT/$FILEDATE.wav.gpg.par2" | grep "All files are correct, repair is not required") ]]; then
-	echo ; success "Par2 verified!"
+if [[ $(par2 verify "$MNTPT/$(date '+%Y-%m-%d')/$FILEDATE.wav.gpg.par2" | grep "All files are correct, repair is not required") ]]; then
+	success "Par2 verified"
 else
-	echo ; error "Par2 verification failed!"
+	error "Par2 verification failed"
 fi
 
 ##################################### Backup recordings
@@ -180,9 +179,9 @@ else
 fi
 
 if [ "$LOCALSTORAGEUSED" -le 2000 ]; then
-        echo ; error "Less then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
+        error "Less then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
 else
-        echo ; success "More then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
+        success "More then 2000MB available on the local storage directory: $LOCALSTORAGEUSED MB (Not USB)"
         rsync -aAXHv "$MNTPT"/ "$LOCALSTORAGE"/
 fi      
 
@@ -190,34 +189,34 @@ fi
 MNTPTR=$(find /mnt -iname '.active' | sed 's|/Recordings/.active||g')
 sync
 sleep 3
-#################################################echo ; systemd-umount "$MNTPTR"
+#################################################systemd-umount "$MNTPTR"
 if [ $? -eq 0 ]; then
-    echo ; success "Systemd-unmount done"
+    success "Systemd-unmount done"
     # Remove folder after unmount
     sleep 2
-	#######################################################rmdir "$MNTPTR" &&  echo ; success "$MNTPTR folder removed" || echo ; error "$MNTPTR folder remove failed"
+	rmdir "$MNTPTR" && success "$MNTPTR folder removed" || error "$MNTPTR folder remove failed"
 else
-	echo ; error "Systemd-umount failed"
-	#####################################################echo ; umount -l "$MNTPTR" && success "umount done" || echo ; success "Umount - Not mounted double check, done"
-	#echo ; systemctl disable "$MOUNT_DIR-$DEVICE".mount && success "Systemctl disabled $MOUNT_DIR-$DEVICE.mount done" || echo ; error "Systemctl disabled $MOUNT_DIR-$DEVICE.mount failed"
-	#echo ; systemctl daemon-reload && success "Systemctl daemon-reload done" || echo ; error "Systemctl daemon-reload failed"
-	#echo ; systemctl reset-failed && success "Systemctl reset-failed done" || echo ; error "Systemctl reset-failed failed"
+	error "Systemd-umount failed"
+	umount -l "$MNTPTR" && success "umount -l done" || error "Umount -l - Not mounted double check, done"
+	#systemctl disable "$MOUNT_DIR-$DEVICE".mount && success "Systemctl disabled $MOUNT_DIR-$DEVICE.mount done" || error "Systemctl disabled $MOUNT_DIR-$DEVICE.mount failed"
+	#systemctl daemon-reload && success "Systemctl daemon-reload done" || error "Systemctl daemon-reload failed"
+	#systemctl reset-failed && success "Systemctl reset-failed done" || error "Systemctl reset-failed failed"
 	# Remove folder after unmount
 	sleep 2
-	####################################################rmdir "$MNTPTR" &&  echo ; success "$MNTPTR folder removed" || echo ; error "$MNTPTR folder remove failed"
+	rmdir "$MNTPTR" && success "$MNTPTR folder removed" || error "$MNTPTR folder remove failed"
 fi	
 
 # test that this device has disappeared from mounted devices
 device_mounted=$(grep -q "$DEV" /etc/mtab)
 if [ "$device_mounted" ]; then
-	echo ; error "Failed to Un-Mount, forcing umount -l"
+	error "Failed to Un-Mount, forcing umount -l"
 	echo "temp disable of unmount for dev"
-	#############################################################umount -l "/dev/$DEVICE"
+	umount -l "/dev/$DEVICE" && success "umount -l done" || error "Umount -l - Not mounted double check, done"
 	if [ $? -eq 0 ]; then
-		rmdir "$MNTPTR" &&  echo ; success "$MNTPTR folder removed" || echo ; error "$MNTPTR folder remove failed"
+		rmdir "$MNTPTR" && success "$MNTPTR folder removed" || error "$MNTPTR folder remove failed"
 	fi
 else
-	echo ; success "Device not present in /etc/mtab"
+	success "Device not present in /etc/mtab"
 fi
 
 ##################################### In progress flag
