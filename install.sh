0a5ba4791cb102f53e28ecf9b233d23ef38f8d2b
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-10-16 14:47:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,20 +20,20 @@ root_check
 clear
 
 ################################### Prefer IPv4 for apt
-echo ; echo -e "|" "${IBlue}IPv4 APT Preference - $DATE${Color_Off}" >&2 ; echo
+header "IPv4 APT Preference - $DATE"
 if [ -f /etc/apt/apt.conf.d/99force-ipv4 ]; then
-	echo -e "${IYellow}IPv4 Preference already set${Color_Off}" >&2
+	warning "IPv4 Preference already set"
 else
 	echo 'Acquire::ForceIPv4 "true";' >> /etc/apt/apt.conf.d/99force-ipv4
 	if [ $? -eq 0 ]; then
-		echo -e "${IGreen}Set IPv4 Preference - Done${Color_Off}" >&2
+		success "Set IPv4 Preference done"
 	else
-		echo -e "${IRed}Set IPv4 Preference - Failed${Color_Off}" >&2
+		error "Set IPv4 Preference failed"
 	fi
 fi
 
 ################################### Upstart
-echo ; echo -e "|" "${IBlue}Setting up rc.local${Color_Off}" >&2 ; echo
+header "Setting up rc.local"
 if [ -f /etc/rc.local ]; then
 	mv /etc/rc.local /etc/rc.local.backup."$DATE"
 fi
@@ -71,44 +71,44 @@ systemctl start rc-local.service
 if [ -f /etc/rc.local ]; then
 	# Check if the above is succesfull
 	if cat /etc/rc.local | grep -q "$GITDIR/install.sh"; then
-		echo ; echo -e "${IGreen}Setting up rc.local - Created${Color_Off}" >&2
+		echo ; success "Setting up rc.local - Created"
 	else
-		echo ; echo -e "${IRed}Setting up rc.local - Failed, file exists but not the proper content${Color_Off}" >&2
+		echo ; error "Setting up rc.local failed, file exists but not the proper content"
 	fi
 else
-	echo ; echo -e "${IRed}Setting up rc.local - Failed${Color_Off}" >&2
+	echo ; error "Setting up rc.local failed"
 fi
 
 
 ################################### Set timezone based upon WAN ip
-echo ; echo -e "|" "${IBlue}Set timezone based on WAN IP${Color_Off}" >&2 ; echo
+header "Set timezone based on WAN IP"
 timedatectl set-timezone Europe/Amsterdam &> /tmp/.tz || true
 if echo $(cat /tmp/.tz) | grep -q "Failed to connect to bus: No such file or directory"; then
-        echo -e "|" "${IYellow}Timezone set - Failed (first install fails because of dbus dependency. Next run will set the timezone automatically) ${Color_Off}" >&2
+        warning "Timezone set failed (first install fails because of dbus dependency. Next run will set the timezone automatically)"
 else
 	curl -s --location --request GET 'https://api.ipgeolocation.io/timezone?apiKey=bbebedbbace2445386c258c0a472df1c' | jq '.timezone' | xargs timedatectl set-timezone
 	if [ $? -eq 0 ]; then
-		echo -e "${IGreen}Timezone set${Color_Off}" >&2
+		success "Timezone set"
 	else
-		echo -e "${IRed}Timezone set failed${Color_Off}" >&2
+		error "Timezone set failed"
 	fi
 fi
 
 #################################### Update
-echo ; echo -e "|" "${IBlue}Update: OS ${Color_Off}" >&2 ; echo
+header "Update: OS"
 export "DEBIAN_FRONTEND=noninteractive"
 export "DEBIAN_PRIORITY=critical"
-echo -e "|" "${IBlue}Auto clean${Color_Off}" >&2
+header "Auto clean"
 #apt_autoclean & spinner
-echo ; echo -e "|" "${IBlue}Auto purge${Color_Off}" >&2
+header "Auto purge"
 #apt_autoremove & spinner
-echo ; echo -e "|" "${IBlue}Update${Color_Off}" >&2
+header "Update"
 #apt_update & spinner
-echo ; echo -e "|" "${IBlue}Upgrade${Color_Off}" >&2
+header "Upgrade"
 #apt_upgrade & spinner
 
 ################################### Dependencies
-echo ; echo -e "|" "${IBlue}Dependencies${Color_Off}" >&2
+header "Dependencies"
 apt-get install -y -qq \
         git \
         jq \
@@ -121,124 +121,124 @@ apt-get install -y -qq \
         ufw \
         rsyslog \
         fail2ban \
-	dbus \
-	lshw \
-	ufw \
-	rsync \
-	par2 \
-	gnupg1	
-	if [ $? -eq 0 ]; then
-		echo ; echo -e "${IGreen}Packages install - Done${Color_Off}" >&2 ; echo
-	else
-		echo ; echo -e "${IRed}Packages install - Failed${Color_Off}" >&2 ; echo
-	fi	
+		dbus \
+		lshw \
+		ufw \
+		rsync \
+		par2 \
+		gnupg1	
+		if [ $? -eq 0 ]; then
+			echo ; success "Packages install done"
+		else
+			echo ; error "Packages install failed"
+		fi	
 
 ################################### VDMFEC
-#echo ; echo -e "|" "${IBlue}VMDFEC${Color_Off}" >&2 ; echo
+#header "VMDFEC"
 apt list vdmfec > /tmp/.vdm 2>&1 || true
 if echo $(cat /tmp/.vdm) | grep -q installed; then
-	echo -e "${IYellow}vdmfec is already installed${Color_Off}" >&2
+	warning "vdmfec is already installed"
 else
 	# 64Bit change for other arm distros
-	wget http://ftp.de.debian.org/debian/pool/main/v/vdmfec/vdmfec_1.0-2+b2_arm64.deb && dpkg -i vdmfec_1.0-2+b2_arm64.deb && rm vdmfec_1.0-2+b2_arm64.deb
+	wget 'http://ftp.de.debian.org/debian/pool/main/v/vdmfec/vdmfec_1.0-2+b2_arm64.deb' && dpkg -i 'vdmfec_1.0-2+b2_arm64.deb' && rm 'vdmfec_1.0-2+b2_arm64.deb'
 	apt list vdmfec > /tmp/.vdm 2>&1 || true
 	if echo $(cat /tmp/.vdm) | grep -q installed; then
-		echo ; echo -e "${IGreen}vdmfec install - Done${Color_Off}" >&2
+		echo ; success "vdmfec install done"
 	else
-		echo ; echo -e "${IRed}vdmfec install - Failed${Color_Off}" >&2
+		echo ; error "vdmfec install failed"
 	fi
 fi
 
 ################################### Allow access, temp during dev
-echo ; echo -e "|" "${IBlue}Dev access${Color_Off}" >&2 ; echo
+header "Dev access"
 if [ -d "/root/.ssh" ]; then
-	echo -e "${IYellow}Folder .ssh exists${Color_Off}" >&2 ; echo
+	warning "Folder .ssh exists"
 else
 	mkdir -p /root/.ssh
-	echo -e "${IGreen}Folder /root/.ssh created${Color_Off}" >&2
+	success "Folder /root/.ssh created"
 fi
 
 if cat /root/.ssh/authorized_keys | grep -q "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1ME48x4opi86nCvc6uT7Xz4rfhzR5/EGp24Bi/C21UOyyeQ3QBIzHSSBAVZav7I8hCtgaGaNcIGydTADqOQ8lalfYL6rpIOE3J4XyReqykLJebIjw9xXbD4uBx/2KFAZFuNybCgSXJc1XKWCIZ27jNpQUapyjsxRzQD/vC4vKtZI+XzosqNjUrZDwtAqP74Q8HMsZsF7UkQ3GxtvHgql0mlO1C/UO6vcdG+Ikx/x5Teh4QBzaf6rBzHQp5TPLWXV+dIt0/G+14EQo6IR88NuAO3gCMn6n7EnPGQsUpAd4OMwwEfO+cDI+ToYRO7vD9yvJhXgSY4N++y7FZIym+ZGz"; then
-	echo -e "${IYellow}Public key exists already${Color_Off}" >&2
+	warning "Public key exists already"
 else
 	echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1ME48x4opi86nCvc6uT7Xz4rfhzR5/EGp24Bi/C21UOyyeQ3QBIzHSSBAVZav7I8hCtgaGaNcIGydTADqOQ8lalfYL6rpIOE3J4XyReqykLJebIjw9xXbD4uBx/2KFAZFuNybCgSXJc1XKWCIZ27jNpQUapyjsxRzQD/vC4vKtZI+XzosqNjUrZDwtAqP74Q8HMsZsF7UkQ3GxtvHgql0mlO1C/UO6vcdG+Ikx/x5Teh4QBzaf6rBzHQp5TPLWXV+dIt0/G+14EQo6IR88NuAO3gCMn6n7EnPGQsUpAd4OMwwEfO+cDI+ToYRO7vD9yvJhXgSY4N++y7FZIym+ZGz" > /root/.ssh/authorized_keys
 	if [ $? -eq 0 ]; then
-		echo -e "${IGreen}Pubkey added${Color_Off}" >&2
+		success "Pubkey added"
 	else
-		echo -e "${IRed}Pubkey appending failed${Color_Off}" >&2
+		error "Pubkey appending failed"
 	fi
 fi
 
 ################################### Create user
-echo ; echo -e "|" "${IBlue}Creating user${Color_Off}" >&2 ; echo
+header "Creating user"
 if cat /etc/passwd | grep "$USERNAME"; then
-	echo ; echo -e "${IYellow}User exists${Color_Off}" >&2
+	echo ; warning "User exists"
 else
 	/usr/bin/sudo useradd -m -p $(openssl passwd -crypt "$PASSWORD") "$USERNAME"
 	if [ $? -eq 0 ]; then
-		echo -e "${IGreen}User added${Color_Off}" >&2
+		success "User added"
 	else
-		echo -e "${IRed}User add failed${Color_Off}" >&2
+		error "User add failed"
 	fi
 fi
 
 ################################### Clone git repo
-echo ; echo -e "|" "${IBlue}Clone git repo${Color_Off}" >&2 ; echo
+header "Clone git repo"
 if [ -d "$GITDIR" ]; then
-	#cd "$GITDIR"
-	#git pull
-	rm -r "$GITDIR"
+	#rm -r "$GITDIR"
+	cd "$GITDIR"
+	git pull
 	if [ $? -eq 0 ]; then
-		echo -e "${IYellow}Git repository removed${Color_Off}" >&2 ; echo 
+		warning "Git repository updated" 
 	else
-		echo -e "${IRed}Git repository failed to remove${Color_Off}" >&2 ; echo
+		error "Git repository update failed"
 	fi
 
 	git clone "$REPO" "$GITDIR"
 	chmod +x "$GITDIR"/scripts/*.sh
 	if [ $? -eq 0 ]; then
-		echo -e "${IGreen}Git repository cloned${Color_Off}" >&2
+		success "Git repository cloned"
 	else
-		echo -e "${IRed}Git repository failed to clone${Color_Off}" >&2
+		error "Git repository failed to clone"
 	fi
 else
 	git clone "$REPO" "$GITDIR"
 	chmod +x "$GITDIR"/scripts/*.sh
 	if [ $? -eq 0 ]; then
-		echo -e "${IGreen}Git repository cloned${Color_Off}" >&2
+		success "Git repository cloned"
 	else
-		echo -e "${IRed}Git repository failed to clone${Color_Off}" >&2
+		error "Git repository failed to clone"
 	fi
 fi
 
 ################################### Hardening
-echo ; echo -e "|" "${IBlue}Hardening${Color_Off}" >&2 ; echo
+header "Hardening"
 /bin/bash "$GITDIR"/scripts/hardening.sh
 if [ $? -eq 0 ]; then
-	echo ; echo -e "${IGreen}Hardening executed${Color_Off}" >&2
+	echo ; success "Hardening executed"
 else
-	echo ; echo -e "${IRed}Hardening failed${Color_Off}" >&2
+	echo ; error "Hardening failed"
 fi
 
 ################################### Dynamic overclock
 # Please at minimum add some heat sinks to the RPI. Better to also add a FAN. thermal throtteling is in place at 75 celcius 
 # Overclocking dynamically will only affect the temp on high load for longer periods. You can mitigate that with above.
-echo ; echo -e "|" "${IBlue}Overclock${Color_Off}" >&2 ; echo
+header "Overclock"
 if cat /proc/cpuinfo | grep -q "Raspberry Pi 4"; then
 	/bin/bash "$GITDIR"/scripts/overclock.sh
 	if [ $? -eq 0 ]; then
-		echo -e "${IGreen}Overclock set, active on next reboot. Press shift during boot to disable${Color_Off}" >&2
+		success "Overclock set, active on next reboot. Press shift during boot to disable"
 	else
-		echo -e "${IRed}Overclock set failed${Color_Off}" >&2
+		error "Overclock set failed"
 	fi
 fi
 
 ################################### GPG
-echo ; echo -e "|" "${IBlue}GPG keys${Color_Off}" >&2 ; echo
+header "GPG keys"
 if gpg1 --homedir /root/.gnupg --list-key | grep -q "${GPG_RECIPIENT}"; then
-	echo -e "${IYellow}GPG key exist${Color_Off}" >&2
+	warning "GPG key exist"
 else
-	echo -e "|" "${IBlue}GPG key creation${Color_Off}" >&2 ; echo 
+	echo -e "|" "${IBlue}GPG key creation" 
 	#GPG_RECIPIENT="recorder@waaromzomoeilijk.nl"
 	gpg1 --homedir /root/.gnupg --list-keys
 
@@ -277,41 +277,41 @@ EOF
 	rm keydetails.asc
 	
 	if gpg1 --homedir /root/.gnupg --list-key | grep -q "${GPG_RECIPIENT}"; then
-		echo -e "${IYellow}GPG key created${Color_Off}" >&2
+		warning "GPG key created"
 	else
-		echo -e "${IRed}GPG key creation failed${Color_Off}" >&2 ; echo 
+		error "GPG key creation failed" 
 	fi	
 fi
 
 ################################### Storage, add auto mount & checks for usb drives
-echo ; echo -e "|" "${IBlue}Storage${Color_Off}" >&2 ; echo
+header "Storage"
 if [ -f "/etc/udev/rules.d/85-usb-loader.rules" ]; then
-	echo -e "${IYellow}/etc/udev/rules.d/85-usb-loader.rules exists${Color_Off}" >&2
+	warning "/etc/udev/rules.d/85-usb-loader.rules exists"
 else
 cat >> /etc/udev/rules.d/85-usb-loader.rules <<EOF
 ACTION=="add", KERNEL=="sd*[0-9]", SUBSYSTEMS=="usb", RUN+="$GITDIR/scripts/usb-initloader.sh ADD %k \$env{ID_FS_TYPE}"
 ACTION=="remove", KERNEL=="sd*[0-9]", SUBSYSTEMS=="usb", RUN+="$GITDIR/scripts/usb-initloader.sh %k"
 EOF
-	udevadm control --reload-rules && echo -e "${IGreen}Storage automation has been setup${Color_Off}" >&2 || echo -e "${IGreen}Storage automation setup has failed${Color_Off}" >&2
+	udevadm control --reload-rules && success "Storage automation has been setup" || warning "Storage automation setup has failed"
 fi
 
 ################################### UPS
-#echo ; echo -e "|" "${IBlue}UPS${Color_Off}" >&2 ; echo
+#header "UPS"
 #/bin/bash "$GITDIR"/scripts/ups.sh
 
 ################################### ZeroTier
-#echo ; echo -e "|" "${IBlue}Zerotier/networking${Color_Off}" >&2 ; echo
+#header "Zerotier/networking"
 #/bin/bash "$GITDIR"/scripts/zerotier.sh
 
 ################################### LED / Buttons
-#echo ; echo -e "|" "${IBlue}LED/buttons${Color_Off}" >&2 ; echo
+#header "LED/buttons"
 #/bin/bash "$GITDIR"/scripts/ph.sh
 
 ################################### 
-echo "Installed on$(date)" > /opt/.rpi-audio-install.sh-finished
+echo "Install script finished on: $(date)" >> /opt/.rpi-audio-install.sh-finished
 
 ################################### Audio recording
-echo ; echo -e "|" "${IBlue}Audio${Color_Off}" >&2 ; echo
+header "Audio"
 echo -e "|" "${IBlue}Audio recording${Color_Off} |" > "$LOG_FILE_AUDIO"
 /bin/bash "$GITDIR"/scripts/audio.sh >> "$LOG_FILE_AUDIO" 2>&1&
 
