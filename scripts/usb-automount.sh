eca03563d614966e3c1f9d457c8c3f0a81b9d749
WaaromZoMoeilijk <info@waaromzomoeilijk.nl>
2021-12-07 09:55:58 +0100

    Update usb-automount.sh

--- scripts/usb-automount.sh
+++ scripts/usb-automount.sh
@@ -11,16 +11,16 @@ source <(curl -sL https://raw.githubusercontent.com/WaaromZoMoeilijk/rpi-audio/m
 # 1 = ON / 0 = OFF
 DEBUG=0
 debug_mode
+
 ################################### Options
 LOG_FILE="$1"
 MOUNT_DIR="$2"
-DEVICE="$3"  # USB device name (from kernel parameter passed from rule)
-FILESYSTEM="$4"
-AUTO_START="$5" 
+
 ################################### check input parameters
 [ "$MOUNT_DIR" ] || fatal "Missing Parameter: MOUNT_DIR"
 [ "$DEVICE" ] || fatal "Missing Parameter: DEVICE"
 [ "$FILESYSTEM" ] || fatal "Missing Parameter: FILESYSTEM"
+
 ################################### check defined log file
 if [ -b /dev/"$DEVICE" ]; then
 	echo "###########  Start usb-automount.sh  $(date)   ############"
@@ -33,11 +33,13 @@ fi
 
 ################################### Mount & log
 automount >> "$LOG_FILE_AUTOMOUNT" 2>&1
+
 ###################################  Auto start & log
 if [ "$AUTO_START" == "1" ]; then # Do we want to auto-start a new process? 0 - No; 1 - Yes
 	echo ; echo -e "|" "${IBlue} --- USB Auto start script --- ${Color_Off} |" >&2 ; echo
 	autostart >> "$LOG_FILE_AUTOSTART" 2>&1
 	echo "###########  END usb-automount.sh  $(date)   ############"
 fi
+
 ################################### Cleanup & exit
 exit 0
